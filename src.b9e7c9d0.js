161e6e059925d5f890fa3926ba9227b826efceef
Konstantin-Kolom <77701872+Konstantin-Kolom@users.noreply.github.com>
2021-06-17 14:54:05 +0000

    Deploying to gh-pages from @ Konstantin-Kolom/goit-js-hw-09-gallery@5380b9f53f8cea9bef98cf3dd7ef58d1dc047387 🚀

--- src.b9e7c9d0.js
+++ src.b9e7c9d0.js
@@ -7,4 +7,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./gallery-items":"q5Rn"}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/tasc08.js");
 },{"./sass/main.scss":"clu1","./js/tasc08.js":"nWBi"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-09-galler/src.42c52e12.js.map
+//# sourceMappingURL=/goit-js-hw-09-galler/src.b9e7c9d0.js.map
